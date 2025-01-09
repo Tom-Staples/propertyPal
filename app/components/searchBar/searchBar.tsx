526c5fb51638555c4728e9e517cce31a654da967
Tom Staples <tom.staples.95@gmail.com>
2025-01-09 13:22:49 +0000

    Initial SearchBar component with styling & basic state config
    *Colour is subject to change

---
+++ app/components/searchBar/searchBar.tsx
@@ -0,0 +1,36 @@
+'use client';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+const SearchBar = () => {
+  const [focusClass, setFocusClass] = useState<string>('border-slate-200');
+  const [searchValue, setsearchValue] = useState<string>('');
+
+  return (
+    <div className={`w-80 rounded-md border-2 flex items-center ${focusClass}`}>
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        size='xs'
+        className='grow-0 px-1'
+      />
+      <input
+        type='text'
+        placeholder='Find something'
+        className=' grow rounded-r-md outline-none'
+        onFocus={() => {
+          setFocusClass('border-orange-300');
+        }}
+        onBlur={() => {
+          setFocusClass('border-slate-200');
+        }}
+        onChange={e => {
+          setsearchValue(e.target.value);
+        }}
+        value={searchValue}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
