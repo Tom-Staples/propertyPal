8fa5b3df07ba93f5bae031646d981c735af69a62
Tom Staples <tom.staples.95@gmail.com>
2025-01-13 12:21:22 +0000

    Removed Notification, message & settings icon from icon bar
    *Relocated to left nav bar

--- app/components/iconBar/iconBar.tsx
+++ app/components/iconBar/iconBar.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import NotificationIcon from '@/components/notificationIcon';
-import SettingsIcon from '@/components/settingsIcon';
 import AddPropertyIcon from '@/components/addPropertyIcon';
-import MessageIcon from '@/components/messgaeIcon';
 
 const IconBar = () => {
   const listStyling = 'px-4 hover:scale-110 hover:opacity-80';
 
   return (
     <ul className='flex justify-end w-80 pr-2'>
-      <li className={listStyling}>
-        <NotificationIcon />
-      </li>
-      <li className={listStyling}>
-        <MessageIcon />
-      </li>
-      <li className={listStyling}>
-        <SettingsIcon />
-      </li>
       <li className={listStyling}>
         <AddPropertyIcon />
       </li>
