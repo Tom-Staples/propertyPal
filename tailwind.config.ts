b6872c61144c2877c8170f9c306208d9341c3b6f
Tom Staples <tom.staples.95@gmail.com>
2025-01-15 15:04:46 +0000

    Tailwind screen breakpoint config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '760px',
+      lg: '1280px',
+      xl: '1510px'
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
