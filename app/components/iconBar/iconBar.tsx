4ad11345e43c730e0f30dcc651946db94ab11b21
Tom Staples <tom.staples.95@gmail.com>
2025-01-20 13:15:50 +0000

    Removed opacity & scale hover effects from hamburger menu

--- app/components/iconBar/iconBar.tsx
+++ app/components/iconBar/iconBar.tsx
@@ -5,7 +5,7 @@ import HamburgerMenu from '../hamburgerMenu';
 const IconBar = () => {
   return (
     <ul className='flex justify-end pr-2'>
-      <li className='md:hidden p-2 hover:scale-105 hover:opacity-90 py-2'>
+      <li className='md:hidden p-2'>
         <HamburgerMenu />
       </li>
       <li className='hidden md:block px-4 hover:scale-105 hover:opacity-90 py-2 rounded-lg bg-orange-300'>
