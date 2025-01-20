91117ebf86e8e704284adbfea0ae94a91b2f705e
Tom Staples <tom.staples.95@gmail.com>
2025-01-20 12:46:40 +0000

    Remove horizontal scrolling for small devices & apply dynamic height
    Scrolling removed to hide sliding Hamburger Menu on small device widths, global text colour applied to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body>
-        <div className='grid grid-rows-12 grid-cols-1 md:grid-cols-12 xl:grid-cols-10 gap-x-2 h-screen bg-slate-50'>
+    <html
+      lang='en'
+      className='overflow-x-hidden md:overflow-x-auto h-dvh md:h-full'
+    >
+      <body className='relative md:static overflow-x-hidden md:overflow-x-auto h-dvh md:h-full'>
+        <div className='grid grid-rows-12 grid-cols-1 md:grid-cols-12 xl:grid-cols-10 gap-x-2 bg-slate-50 h-full text-slate-600'>
           <Header />
           {children}
         </div>
