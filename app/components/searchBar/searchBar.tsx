2952a9ce761918b02524c08f2a6099824f7e55de
Tom Staples <tom.staples.95@gmail.com>
2025-01-29 13:47:24 +0000

    Remove focus state from SearchBar component & use focus-within: pseudo class

--- app/components/searchBar/searchBar.tsx
+++ app/components/searchBar/searchBar.tsx
@@ -4,15 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = () => {
-  const [focusClass, setFocusClass] = useState<string>(
-    'border-white md:border-slate-200'
-  );
   const [searchValue, setsearchValue] = useState<string>('');
 
   return (
-    <div
-      className={`flex bg-white w-40 sm:w-80 border-2 rounded-md items-center ${focusClass}`}
-    >
+    <div className='flex bg-white w-40 sm:w-80 border-2 rounded-md items-center border-white md:border-slate-200 focus-within:border-slate-600 focus-within:md:border-orange-300'>
       <FontAwesomeIcon
         icon={faMagnifyingGlass}
         size='xs'
@@ -22,12 +17,6 @@ const SearchBar = () => {
         type='text'
         placeholder='Find something'
         className='w-32 text-sm sm:text-base sm:w-auto rounded-r-md outline-none p-2'
-        onFocus={() => {
-          setFocusClass('border-slate-600 md:border-orange-300');
-        }}
-        onBlur={() => {
-          setFocusClass('border-white md:border-slate-200');
-        }}
         onChange={e => {
           setsearchValue(e.target.value);
         }}
